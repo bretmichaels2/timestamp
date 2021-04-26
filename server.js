0b6eabb1888b013655970b64c34573dbdcaad85f
bretmichaels2 <ksucunning@gmail.com>
2021-04-26 18:54:30 -0400

    moved stuff to docs

--- server.js
+++ server.js
@@ -32,11 +32,11 @@ var cors = require('cors');
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(express.static('docs'));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+  res.sendFile(__dirname + '/docs/index.html');
 });
 
 // listen for requests :)
