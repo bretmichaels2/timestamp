a04f4472e7f09f294422e5bbcdb62ea0fc611285
bretmichaels2 <ksucunning@gmail.com>
2021-04-26 18:51:31 -0400

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,27 @@
 var express = require('express');
 var app = express();
 
+app.get("/api/timestamp/", (req, res) => {
+  res.json({ unix: Date.now(), utx: Date() })
+});
+
+app.get("/api/timestamp/:date_string", (req, res) => {
+  let dateString = req.params.date_string;
+  if (/\d{5,}/.test(dateString)) {
+    const dateInt = parseInt(dateString);
+    res.json({ unix: dateInt, utc: new Date(dateInt).toUTCString() })
+  }
+  else {
+    let dateObject = new Date(dateString);
+    if (dateObject.toString() === "Invalid Date") {
+      res.json({ error: "Invalid Date"})
+    }
+    else {
+      res.json({ unix: dateObject.valueOf(), utc: dateObject.toUTCString() })
+    }
+  }
+});
+
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
@@ -17,14 +38,6 @@ app.use(express.static('public'));
 app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
-
-
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
-});
-
-
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
